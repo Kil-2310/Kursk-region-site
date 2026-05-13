9544438c3192ea5d40767b106d057612a85cc451
kil-2310 <homakove600@gmail.com>
2026-05-13 12:55:45 +0300

    Remove backend

--- backend/app/middleware/__init__.py
+++
@@ -1,2 +0,0 @@
-from .analytics import setup_profiler
-from .protection import limiter, setup_cors_protect, setup_open_api
--- backend/app/middleware/protection/__init__.py
+++
@@ -1,3 +0,0 @@
-from .cors import setup_cors_protect
-from .open_api import setup_open_api
-from .throttle import limiter
